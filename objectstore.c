fdda28c9f314ace70b7760264aa013e7a476c08d
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-06 10:39:29 +0200

    started

---
+++ objectstore.c
@@ -0,0 +1,29 @@
+#define _POSIX_C_SOURCE 200112L
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+
+#include<util.h>
+
+int main(int argc, char *argv[]){
+    /*creazione della server socket */
+    int serverfd;
+    CHECK(serverfd, socket(AF_UNIX, SOCK_STREAM, 0), "socket");
+
+    struct sockaddr_un ssock_addr;
+    memset(&ssock_addr, '0', sizeof(ssock_addr));
+    ssock_addr.sun_family=AF_UNIX;
+    strncpy(ssock_addr.sun_path, SOCKNAME, strlen(SOCKNAME)+1);
+
+    int p;
+    CHECK(p, bind(serverfd, (struct sockaddr*)&ssock_addr, sizeof(ssock_addr)), "bind");
+    CHECK(p, listen(serverfd, SOMAXCONN), "listen");
+    
+
+    return 0;
+}
---
+++ util.h
@@ -0,0 +1,47 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <sys/types.h>
+#include <sys/un.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SOCKNAME "./objectstore.sock"
+#define True 1
+#define False 0
+
+#define CHECK(x,f,m) \
+    if((x=f)==-1){   \
+        perror(m);   \
+        exit(errno); \
+    }
+
+#define ISNULL(x,f,m) \
+    if((x=f)==NULL){  \
+        perror(m);    \
+        exit(errno);  \
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  /* UTIL_H */
