06cc53c75039518862db32240d7a8dd4261b69d0
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-07 10:16:15 +0200

    First commit

---
+++ worker.c
@@ -0,0 +1,14 @@
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <worker.h>
+
+void *Worker(void *arg){
+
+
+}
+
+int main(){
+    return 0;
+}
---
+++ worker.h
@@ -0,0 +1,9 @@
+#ifndef WORKER_H
+#define WORKER_H
+
+#define _POSIX_C_SOURCE 200112L
+#include<util.h>
+
+void *Worker(void *arg);
+
+#endif
