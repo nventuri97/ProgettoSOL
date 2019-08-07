d9872dd2d28afe1be956a7a9ba73595f2509a802
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-07 17:32:37 +0200

    work in progress

--- client.c
+++ client.c
@@ -12,27 +12,53 @@
 #include <util.h>
 #include <access.h>
 
+#define MAXNAME 100
+#define MAXOBJNAME 100
+
 static int success, failure, tot_test;
+
+void store_test(char *objname);
+void retrive_test(char *objname);
+void delte_test(char *objname);
 
 int main(int argc, char *argv[]){
     if(argc<4){
         fprintf(stderr, "Use, %s name obj action\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    /*name lenght, object name lenght */
+    int nl, onl;
+
+    if((nl=strlen(argv[1]))>MAXNAME){
+        fprintf(stderr, "Il nome deve essere < di %d\n", MAXNAME);
+        return EXIT_FAILURE;
+    }
+    if((onl=strlen(argv[2]))>MAXOBJNAME){
+        fprintf(stderr, "Il nome dell'oggetto deve essere < di %d\n", MAXOBJNAME);
+        return EXIT_FAILURE;
+    }
+
+    char *name, *objname;
+    name=calloc(nl, sizeof(char));
+    objname=calloc(onl, sizeof(char));
+    strcpy(name,argv[1]);
+    strcpy(objname, argv[2]);
+
+    int err=os_connect(name);
+    int action=atoi(argv[3]);
 
     success=0;
     failure=0;
     tot_test=0;
-    int err=os_connect(argv[1]);
-    int action=atoi(argv[3]);
+    
 
     if(err==True){
         if(action==1)
-            store_test(argv[2]);
+            store_test(objname);
         else if(action==2)
-            retrieve_test(argv[2]);
+            retrieve_test(objname);
         else if(action==3)
-            delete_test(argv[2]);
+            delete_test(objname);
         else{
             fprintf(stderr, "Action deve essere compreso tra 1 e 3\n");
             exit(EXIT_FAILURE);
