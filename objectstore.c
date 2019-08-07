e0909cfbdda719aa40f8bd4d9486b8f728002b02
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-07 10:03:01 +0200

    work in progress

--- objectstore.c
+++ objectstore.c
@@ -7,8 +7,16 @@
 #include <sys/wait.h>
 #include <sys/uio.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 
 #include<util.h>
+
+/*variabili condivise tra il main e i vari thread worker */
+static pthread_mutex_t mtx=PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t mod=PTHREAD_COND_INITIALIZER;
+
+//#client connessi, size totale dell'object store, #totale di oggetti
+static int conn_client, tot_size, n_obj;
 
 int main(int argc, char *argv[]){
     /*creazione della server socket */
@@ -24,9 +32,22 @@ int main(int argc, char *argv[]){
     CHECK(p, bind(serverfd, (struct sockaddr*)&ssock_addr, sizeof(ssock_addr)), "bind");
     CHECK(p, listen(serverfd, SOMAXCONN), "listen");
 
-    /*dichiaro i thread dispatcher e worker */
-    pthread_t os_dispatcher, os_worker;
-    CHECK(p, pthread_create(os_dispatcher,NULL, dispatcher, NULL), "thread create");
-    /*DEVO ANCORA DEFINIRE LA DISPATCHER NELLA LIBRERIA!!!!!!!!!!!!! */
+    /*creazione della cartella DATA */
+    CHECK(p, mkdir("data", 0700), "mkdir");
+
+    /*inizializzo le variabili globali tutte a 0*/
+    conn_client=0;
+    tot_size=0;
+    n_obj=0;
+
+    /*dichiaro il worker */
+    pthread_t os_worker;
+
+    int clientfd;
+    do{
+        CHECK(clientfd, accept(serverfd, (struct sockaddr *)NULL, NULL), "accept");
+        CHECK(p, pthread_create(&os_worker, NULL, worker, NULL), "pthread create");
+    }while(True);
+    
     return 0;
 }
--- util.h
+++ util.h
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #define SOCKNAME "./objectstore.sock"
 #define True 1
