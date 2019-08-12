279f988e46d58773d9e10e3625947166115f683e
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-12 16:47:52 +0200

    Work in progress

--- worker.c
+++ worker.c
@@ -19,10 +19,15 @@ void *Worker(int client_fd){
     new_worker->pid=getpid();
     new_worker->workerfd=client_fd;
     worker_l=new_worker;
-    ready=1;
 
+    ready=1;
     pthread_cond_signal(&mod);
     pthread_mutex_unlock(&mtx);
+    
+    /*alloco in questo modo poiché le parole chiave hanno lunghezza massima di 8 più uno spazio*/
+    char cl_msg[9+MAXNAME+1];
+    int p;
+    CHECK(p, readn(client_fd, cl_msg, strlen(cl_msg)*sizeof(char)+1), "readn");
     
 }
 
