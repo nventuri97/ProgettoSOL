0177c54e389dd3f10b765b1b2508df9fd66375c4
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-21 11:59:00 +0200

    Correzioni per eliminare warning

--- util.h
+++ util.h
@@ -4,13 +4,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<pthread.h>
 #include<sys/types.h>
 #include<sys/un.h>
+#include<sys/stat.h>
 #include<errno.h>
-#include<pthread.h>
 #include<unistd.h>
 #include<signal.h>
-#include<sys/stat.h>
+
 
 #define SOCKNAME "./objstore.sock"
 #define True 1
