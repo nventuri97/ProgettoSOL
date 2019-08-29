1c39e06fa6c15fd273dc5634b596bb009b0db694
nventuri97 <n.venturi3@studenti.unipi.it>
2019-08-29 11:01:35 +0200

    Gestione dei segnali, inizio

---
+++ signal_t.c
@@ -0,0 +1,2 @@
+#define _POSIX_C_SOURCE 200809L
+#include"signal_t.h"
---
+++ signal_t.h
@@ -0,0 +1,9 @@
+#ifndef SIGNAL_T
+#define SIGNAL_T
+
+#include"util.h"
+#include<signal.h>
+
+pthread_t create_signal_t();
+
+#endif /*SIGNAL_T*/
